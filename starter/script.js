fd3a1468b1316006297d22ec27f30b116ede1236
Dimi Tsonev <dconev@gmail.com>
2023-12-18 18:11:18 +0000

    fixed bugs

--- starter/script.js
+++ starter/script.js
@@ -1,14 +1,11 @@
 const APIKey = "91d963d53697a7a28a646b274c52abda";
 
-
 const today = $('#today')
 const forecast = $('#forecast')
-
 
 // cityName = $('#search-input')
 
 searchBtn = $('#search-button')
-
 
 const currentDay = dayjs();
 const currentTime24 = parseInt(currentDay.format("HH"));
@@ -19,8 +16,13 @@ const showDate = dayjs().format("D/MM/YYYY");
 console.log(showDate)
 
 searchBtn.on('click', function(event){
+
+  $('#today').empty();
+  $('#forecast').empty();
   event.preventDefault(); 
-  cityName = $('#search-input').val();
+  const todayCard = $('<div>').addClass('card  rounded bg-warning')
+
+  cityName = $('#search-input').val()
   console.log(cityName);
 
   const queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${APIKey}`
@@ -44,16 +46,20 @@ searchBtn.on('click', function(event){
       const icon = $('<img>').attr('src', iconPng).attr ('alt',data.weather[0].main)
       // Display city name, date
 
-      const cityAndDate = $('<h3>').text(` ${data.name}  (${showDate})`);
+      const cityAndDate = $('<h3>').text(` ${data.name}  (${showDate})`)
       cityAndDate.append(icon);
-      today.append(cityAndDate);
+      todayCard.append(cityAndDate);
 
+      const todayCardBody = $('<div>').addClass('card-body')
 
       const tempC = (data.main.temp - 273.15).toFixed(2);
       const temp = $("<p>").text("Temperature " + tempC);
       const wind = $("<p>").text("Wind Speed: " + data.wind.speed);
       const humidity = $("<p>").text("Humidity: " + data.main.humidity);
-      today.append(temp,wind,humidity)
+      todayCardBody.append(temp,wind,humidity)
+      todayCard.append(todayCardBody);
+      today.append(todayCard)
+
 
 
     const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
@@ -73,28 +79,28 @@ searchBtn.on('click', function(event){
       const forecastDay = currentDay.add(i, 'day');
       const forecast5Days = forecastList.find(item => dayjs(item.dt_txt).isSame(forecastDay, 'day'));
     
-      // Formatting the forecast date
-      const forecastDayFormatted = forecastDay.format('DD/MMYYYY');
+      //Formatting the forecast date
+      const forecastDayFormatted = forecastDay.format('DD/MM/YYYY');
     
-      // Create a Bootstrap card
+      //Create a Bootstrap card
       const card = $('<div>').addClass('col-md-2 mb-3');
-      const cardBody = $('<div>').addClass('card-body');
+      const cardBody = $('<div>').addClass('card-body rounded card-body text-center bg-warning');
       const forecastDate = $('<h5>').addClass('card-title').text(forecastDayFormatted);
     
-      // Extract temperature, wind, humidity, and icon information
+      //Getting the temperature,wind,humidity and the icon 
       const tempC = (forecast5Days.main.temp - 273.15).toFixed(2);
       const windSpeed = forecast5Days.wind.speed;
       const humidity = forecast5Days.main.humidity;
-      const iconPng = `https://openweathermap.org/img/wn/${forecast5Days.weather[0].icon}@2x.png`;
+      const iconPng = `https://openweathermap.org/img/wn/${forecast5Days.weather[0].icon}@2x.png`
     
       // Create forecast details and icon elements
-      const temp = $("<p>").addClass('card-text small').text(`Temperature: ${tempC}°C`);
-      const wind = $("<p>").addClass('card-text small').text(`Wind Speed: ${windSpeed} m/s`);
-      const humidityInfo = $("<p>").addClass('card-text small').text(`Humidity: ${humidity}%`);
-      const forecastIcon = $('<img>').addClass('card-img-top').attr('src', iconPng).attr('alt', forecast5Days.weather[0].main);
+      const temp = $("<p>").addClass('card-text small').text(`Temperature: ${tempC}°C`)
+      const wind = $("<p>").addClass('card-text small').text(`Wind Speed: ${windSpeed} m/s`)
+      const humidityInfo = $("<p>").addClass('card-text small').text(`Humidity: ${humidity}%`)
+      const forecastIcon = $('<img>').addClass('card-img-top').attr('src', iconPng).attr('alt', forecast5Days.weather[0].main)
     
       // Append elements to the card
-      cardBody.append(forecastDate, forecastIcon, temp, wind, humidityInfo);
+      cardBody.append(forecastDate, forecastIcon, temp, wind, humidityInfo)
       card.append(cardBody);
     
       // Append the card to the forecast container
